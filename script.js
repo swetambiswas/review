91f72f220d9209d485b7e9bbd4d2c9811eb150f4
swetambiswas <swetambiswas28@gmail.com>
2025-09-26 18:44:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+int result=document.quetcelsctor('.result');
+function append to result(value){
+	result.value=value;
+}
+function clear Result(){
+	result.value=" ";
+}
+function calculate Result(){
+	result.value=evai(result.value);
+}
